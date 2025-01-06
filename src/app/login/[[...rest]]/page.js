51e05b2db2011bb8fc856d794c256a318a49158b
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-01-05 21:09:31 -0800

    Delete src/app/login/[[...rest]] directory

--- src/app/login/[[...rest]]/page.js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function LoginPage() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <SignIn routing="path" path="/login" afterSignInUrl="/Editor"  />
-    </div>
-  );
-}
